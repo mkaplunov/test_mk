b87916379d70c01a76ab6217dca00d37dfb3e6f2
Michael Kaplunov <michael.kaplunov@agylstor.com>
2018-04-24 22:33:10 -0700

    another commit

--- Jenkinsfiles/test01.groovy
+++ Jenkinsfiles/test01.groovy
@@ -1,8 +1,8 @@
 def utils
 node {
    echo 'Hello World'
-   String currentDir = new File(".").getAbsolutePath()
-   echo currentDir
+   def scriptDir = getClass().protectionDomain.codeSource.location.path
+   echo scriptDir
    utils = load 'Jenkinsfiles/test02.groovy'
    utils.print_hello_again()
 }
