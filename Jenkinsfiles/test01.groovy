50f99832ebf18024d23a6e42d2da0894c4414bd9
Michael Kaplunov <michael.kaplunov@agylstor.com>
2018-04-24 23:26:08 -0700

    another commit

--- Jenkinsfiles/test01.groovy
+++ Jenkinsfiles/test01.groovy
@@ -1,4 +1,4 @@
-def utils = load("test02.groovy")
+def utils = load("${workspace}@script/Jenkinsfiles/test02.groovy")
 node {
    echo 'Hello World'
    utils.slack_info('Successful test', '#C0C0C0')
